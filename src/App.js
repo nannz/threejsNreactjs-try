5739abd4b0c9fd4bd431389d2c9f8d7c9b52afc5
Nan Zhao <nanzhao@nyu.edu>
2018-02-06 22:03:41 +0800

    01_try to combine threeJS & react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+//the tutorial: about three-render
+// https://www.youtube.com/watch?v=eRMhOpdaKE8
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <p>
+          hello.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
